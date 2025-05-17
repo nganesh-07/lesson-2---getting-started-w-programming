4b0fb7e7c770925e75f5c160caae2033fecfe405
nganesh <nganesh@iu.edu>
2025-05-17 12:25:23 -0700

    print statements

---
+++ print.py
@@ -0,0 +1,10 @@
+# Print a Sentence
+print("Welcome to the world of programming")
+
+print(1457)
+
+print("I love indian clothes \n")
+
+print("My mom has", 2, "kids")
+
+print("My brother likes to eat ", end="pizza \n")
